40240fa10a9cd73f61e22fc95f31460d41b6b6e9
Jesse Hood <jessenoob@gmail.com>
2017-12-13 14:07:23 -0500

    mini sprint

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -4,4 +4,24 @@ export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
     // Add the code for this action
+    const apiUrl = "http://localhost:5000/friends";
+    const friendsRequest = axios.get(apiUrl);
+
+    //redux-promise middleware takes care of resolving the promise
+    return { 
+        type: GET_FRIENDS,
+        payload: friendsRequest
+    };
 };
+
+
+// Methods: 
+// GET: client wants information from the server
+// POST: create a resource
+// PUT: modify information
+// DELETE: remove information
+// most operations are asynchronous
+// Promise: substitute for callbacks 
+let payload = {};
+const apiUrl = 'http://localhost:5000/friends';
+const friendsRequest = axios.get(apiUrl);
